7d6e70a5a0db4c207d2255a47f65fae9af9f9423
Magomed <sultanahmedov19948@yandex.ru>
2024-02-07 13:33:37 +0300

    Lesson_15

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,4 +1,4 @@
-package com.basejava.webapp;
+package com.basejava.webapp.web;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -7,7 +7,12 @@ import java.io.IOException;
 public class ResumeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
     }
 
     @Override
